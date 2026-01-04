cbf87f774d9683bc3766c0bf6d604532aaa4a752
Manuranga <manurangajayawardhana@gmail.com>
2026-01-04 18:47:55 +0530

    feat: Integrate Gemini AI for activity recommendations
    Added GeminiService and ActivityAIService to generate AI-based recommendations for fitness activities. Updated Activity model to include 'type' field. Modified ActivityMessageListener to log AI recommendations. Added WebClient configuration and necessary dependencies for WebFlux. Configured Gemini API properties in application.yaml.

---
+++ aiservice/src/main/java/com/formpilot/aiservice/service/GeminiService.java
@@ -0,0 +1,53 @@
+package com.formpilot.aiservice.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class GeminiService {
+    private final WebClient webClient;
+
+    @Value("${gemini.api.url}")
+    private String geminiApiUrl;
+
+    @Value("${gemini.api.key}")
+    private String geminiApiKey;
+
+    public GeminiService(WebClient.Builder webClientBuilder){
+        this.webClient = webClientBuilder.build();
+    }
+
+    public String getAnswer(String question) {
+        Map<String, Object> requestBody = Map.of(
+                "contents", List.of(
+                        Map.of("parts", List.of(
+                                Map.of("text", question)
+                        ))
+                )
+        );
+
+        return webClient.post()
+                .uri(geminiApiUrl)
+                .header("Content-Type", "application/json") // Ensure this header is set
+                .header("X-goog-api-key", geminiApiKey)
+                .bodyValue(requestBody)
+                .retrieve()
+                .onStatus(HttpStatusCode::is4xxClientError, clientResponse ->
+                        clientResponse.bodyToMono(String.class).flatMap(errorBody -> {
+                            System.err.println("==========================================");
+                            System.err.println("GOOGLE API REJECTED REQUEST: " + errorBody);
+                            System.err.println("==========================================");
+                            return Mono.error(new RuntimeException("Gemini API Error: " + errorBody));
+                        })
+                )
+                .bodyToMono(String.class)
+                .block();
+    }
+}

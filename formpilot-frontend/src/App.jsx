bbf31437c83bc6eef2651eb532d627f17b4419be
Manuranga <manurangajayawardhana@gmail.com>
2026-01-11 08:02:46 +0530

    feat: Add authentication and Redux store setup
    Introduced authentication configuration and state management using Redux Toolkit. Added new dependencies for MUI, Redux, and OAuth2 PKCE. Updated App.jsx and main.jsx to integrate authentication and Redux store.

--- formpilot-frontend/src/App.jsx
+++ formpilot-frontend/src/App.jsx
@@ -1,5 +1,43 @@
+import { Button } from "@mui/material";
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "react-oauth2-code-pkce";
+import { useDispatch } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
+import { setCredentials } from "./store/authSlice";
+
 function App() {
-  return <></>;
+  const { token, tokenData, logIn, logOut, isAuthenticated } =
+    useContext(AuthContext);
+  const dispatch = useDispatch();
+  const [authReady, setAuthReady] = useState(false);
+
+  useEffect(() => {
+    if (token) {
+      dispatch(setCredentials({ token, user: tokenData }));
+      setAuthReady(true);
+    }
+  }, [token, tokenData, dispatch]);
+
+  return (
+    <Router>
+      {!token ? (
+        <Button
+          variant="contained"
+          color="#dc004e"
+          onClick={() => {
+            logIn();
+          }}
+        >
+          LOGIN
+        </Button>
+      ) : (
+        <div>
+          <pre>{JSON.stringify(tokenData, null, 2)}</pre>
+          <pre>{JSON.stringify(token, null, 2)}</pre>
+        </div>
+      )}
+    </Router>
+  );
 }
 
 export default App;

96ec26e6c59a0ee456e56bf8a18cd15d15405429
Manuranga <manurangajayawardhana@gmail.com>
2026-01-24 14:25:05 +0530

    update: Change the message broker from RabbitMQ to Kafka

--- formpilot-backend/activityservice/src/main/java/com/formpilot/activityservice/service/ActivityService.java
+++ formpilot-backend/activityservice/src/main/java/com/formpilot/activityservice/service/ActivityService.java
@@ -6,8 +6,6 @@ import com.formpilot.activityservice.dto.ActivityResponse;
 import com.formpilot.activityservice.model.Activity;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.stereotype.Service;
 
--- formpilot-backend/aiservice/src/main/java/com/formpilot/aiservice/service/ActivityMessageListener.java
+++ formpilot-backend/aiservice/src/main/java/com/formpilot/aiservice/service/ActivityMessageListener.java
@@ -5,11 +5,8 @@ import com.formpilot.aiservice.model.Recommendation;
 import com.formpilot.aiservice.repository.RecommendationRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Service;
 
 import java.util.function.Consumer;
 
